409a32eaca6deff3565b18db1b56d889ab293332
Seitaro Yoneda <m234341@hiroshima-u.ac.jp>
2023-07-05 02:09:34 +0000

    update checkfuntion

--- main.go
+++ main.go
@@ -23,9 +23,17 @@ func (b *Board) get(x, y int) string {
 
 func (b *Board) check() string {
 	var token string
-	token = b.get(0, 0)
-	if token == b.get(1, 0) && token == b.get(2, 0) {
-		return "yes"
+	for y := 0; y < 3; y++ {
+		token = b.get(0, y)
+		if token == b.get(1, y) && token == b.get(2, y) {
+			return "yes"
+		}
+	}
+	for x := 0; x < 3; x++ {
+		token = b.get(x, 0)
+		if token == b.get(x, 1) && token == b.get(x, 2) {
+			return "yes"
+		}
 	}
 	return "no"
 }
--- main_test.go
+++ main_test.go
@@ -24,6 +24,7 @@ func TestPutToken02(t *testing.T) {
 	}
 }
 
+// yoko
 func TestCheck01(t *testing.T) {
 	b := &Board{
 		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -32,6 +33,32 @@ func TestCheck01(t *testing.T) {
 	b.put(1, 0, "o")
 	b.put(2, 0, "o")
 	if b.check() != "yes" {
+		t.Errorf("Mistake")
+	}
+}
+
+// tate
+func TestCheck02(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(1, 0, "x")
+	b.put(1, 1, "x")
+	b.put(1, 2, "x")
+	if b.check() != "yes" {
+		t.Errorf("Mistake")
+	}
+}
+
+// nashi
+func TestCheck03(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(1, 0, "x")
+	b.put(0, 1, "o")
+	b.put(2, 2, "x")
+	if b.check() != "no" {
 		t.Errorf("Mistake")
 	}
 }
